5a8af0aef200e13f77a0146ddbc9ca485b1fd626
holger <holger@surfacepattern.ch>
2014-05-28 23:21:52 +0200

    Fixed javascript, made simple but very direct. Waiting for angular bindig.

--- source/scripts/output.js
+++ source/scripts/output.js
@@ -11,88 +11,29 @@ CASFEE.namespace('CASFEE.output');
 
 /* module pattern, see book "JavaScript Patterns, p. 97f */
 CASFEE.output = (function() {
-    var outputMethods = {
-        articleElement: function(rootNode) {
-            return nodeHelper.appendNode(rootNode, "article");
-        },
-        articleColumnElement: function(rootNode) {
-            return nodeHelper.appendNode(rootNode, "div", "column grid-12");
-        },
-        votingElement: function(rootNode, article) {
-            return nodeHelper.appendNode(rootNode, "div", "article-voting pull-left");
-        },
-        votingContent: function(node, article) {
-            node.innerText = "Vote for " + article.title;
-        },
-        imageElement: function(rootNode, article) {
-            return nodeHelper.appendNode(rootNode, "div", "article-image pull-left");
-        },
-        imageContent: function(imageNode, article) {
-            imageNode.innerText = "IMAGE URL=" + article.image;
-        },
-        articleContentElement: function(rootNode, article) {
-            return nodeHelper.appendNode(rootNode, "div", "article-content");
-        },
-        articleContent: function(node, article) {
-            var innerHtmlMeta,
-                innerHtmlMain,
-                tagsList = "",
-                tagsSize,
-                i;
-            innerHtmlMain = '<div class="article-main">' +
-                '<h3 class="article-heading">' + article.title + '</h3>' +
-                '<p class="article-description">' + article.description + '</p>' +
-                '</div>';
-            if (article.tags !== undefined) {
-                tagsSize = article.tags.length;
-                for (i = 0; i < tagsSize; i++) {
-                    tagsList += article.tags[i];
-                    if(i !== tagsSize) {
-                        tagsList += " / ";
-                    }
-                }
-            } else {
-                tagsList = "";
-            }
-            innerHtmlMeta = '<div class="article_meta">' +
-                '<div class="pull-left"><i class="fa fa-tags"><span class="article-tags">' + tagsList + '</span></i></div>' +
-                '<div class="pull-right"><i class="fa fa-comments-o"><span class="article-comments">comments-popup</span></i></div>' +
-                '<div class="pull-right"><i class="fa fa-user"><span class="article-user">user-popup</span></i></div>' +
-                '<div class="pull-right"><i class="fa fa-calendar"><span class="article-date">date-popup</span></i></div>' +
-                '</div>';
-            node.innerHTML = innerHtmlMain + innerHtmlMeta;
-        }
-    };
-    var nodeHelper = {
-        appendRowToRow: function(rootNode, classes, idName) {
-            return nodeHelper.appendNode(rootNode.parentElement, "div", classes, idName);
-        },
-        appendNode: function(rootNode, nodeType, classes, idName) {
-            var newNode;
-            newNode = document.createElement(nodeType);
-            if (classes !== undefined) {
-                newNode.className = classes;
-            }
-            if (idName !== undefined) {
-                newNode.id = idName;
-            }
-            rootNode.appendChild(newNode);
-            return newNode;
-        }
-    };
-
     // this function builds a complete article
     function buildArticle(article) {
-        var newRow,
-            articleElement,
-            articleColumnElement;
+        var $newRow,
+            voteContent,
+            imageContent,
+            articleContent;
         window.console.log("article is " + article);
-        newRow = nodeHelper.appendRowToRow(document.getElementById("articles_section").lastElementChild, "row");
-        articleElement = outputMethods.articleElement(newRow);
-        articleColumnElement = outputMethods.articleColumnElement(articleElement);
-        outputMethods.votingContent(outputMethods.votingElement(articleColumnElement, article), article);
-        outputMethods.imageContent(outputMethods.imageElement(articleColumnElement, article), article);
-        outputMethods.articleContent(outputMethods.articleContentElement(articleColumnElement, article), article);
+        voteContent = '<div class="article-voting pull-left">Voting</div>';
+        imageContent = '<div class="article-image pull-left">IMG=' + article.image + '</div>';
+        articleContent =
+            '<div class="article-content">' +
+                '<div class="article-main">' +
+                    '<h3 class="article-heading">' + article.title + '</h3>' +
+                    '<p class="article-description">' + article.description + '</p>' +
+                '</div>' +
+                '<div class="article_meta">' +
+                    '<div class="pull-left"><i class="fa fa-tags"><span class="article-tags">' + article.tags.join(' / ') + '</span></i></div>' +
+                    '<div class="pull-right"><i class="fa fa-comments-o"><span class="article-comments">comments-popup</span></i></div>' +
+                    '<div class="pull-right"><i class="fa fa-user"><span class="article-user">user-popup</span></i></div>' +
+                    '<div class="pull-right"><i class="fa fa-calendar"><span class="article-date">date-popup</span></i></div>' +
+                '</div>' +
+            '</div>';
+        $newRow = $('#result').append('<div class="row"><article><div class="column grid-12">' + voteContent + imageContent + articleContent + '</div></article></div>');
     }
 
     return {
